3a68992509c44d29a3a982e6b08e9bdbd6929238
SireGooby <pierrejuopperi@gmail.com>
2017-09-09 18:05:56 +0200

    Added new functions to commit.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,17 @@
-public class Hello
+
+import java.util.Scanner;
+
+public class HelloWorld
 {
 
-  public static void main(string[] args)
+  public static void main(String[] args)
   {
-    System.out.println("Hello, World!");
+    Scanner reader = new Scanner (System.in);
+    System.out.println("Hello, User! Please enter your credentials here:");
+    String name = reader.next();
+    System.out.println("Hello! " + name + ". Please enter your age:");
+    int age = reader.nextInt();
+    System.out.println("You are of age: " + age + ", Correct?");
 
   }
 
