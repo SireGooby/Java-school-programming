70c3bbe940199b212752995bb01e0d26a319b4b1
SireGooby <pierrejuopperi@gmail.com>
2017-09-11 19:38:25 +0200

    Skol uppgifter

---
+++ screensize/screensize.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+
+
+public class screensize extends PApplet
+{
+public static void main(String[] args) 
+{
+        PApplet.main("screensize");
+}
+void setup() {
+  size(200, 200);
+  background(0);
+  noStroke();
+  fill(102);
+}
+}
